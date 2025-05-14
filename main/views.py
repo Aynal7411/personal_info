5109f18733e8e50318eec5fb6e4e46e4b51122c7
Aynal <aynalhaque7411@gmail.com>
2025-05-14 03:36:17 -0700

    register setup

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def register_view(request):
     if request.method == 'POST':
@@ -17,10 +18,23 @@ def register_view(request):
     return render(request, 'register.html')
 
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid username or password.')
     return render(request, 'login.html')
 
 def logout_view(request):
     logout(request)
     messages.info(request, 'Logged out successfully.')
     return redirect('login')
-    return render(request, 'logout.html')
+    
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
